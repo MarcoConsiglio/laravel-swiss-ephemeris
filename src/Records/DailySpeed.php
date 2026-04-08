8ed6227d9d50a966e9efef89e148dd9abe4d18e0
Marco Consiglio <mrccnsgl@gmail.com>
2026-04-08 17:49:46 +0000

    fix: `DailySpeed` constructor

--- src/Records/DailySpeed.php
+++ src/Records/DailySpeed.php
@@ -2,11 +2,18 @@
 namespace MarcoConsiglio\Ephemeris\Records;
 
 use MarcoConsiglio\Goniometry\Angle;
+use MarcoConsiglio\Goniometry\Builders\FromSexadecimal;
+use MarcoConsiglio\Goniometry\SexadecimalDegrees;
 
 class DailySpeed extends Angle
 {
     public function __toString()
     {
-        return "{$this}/day";
+        return parent::__toString();
+    }
+
+    public static function createFromDecimal(float|SexadecimalDegrees $sexadecimal): DailySpeed
+    {
+        return new DailySpeed(new FromSexadecimal($sexadecimal));
     }
 }
